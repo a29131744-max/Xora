48050a8ff754dfc6f0d3c8a8319379a987276ae9
Aditya <a29131744@gmail.com>
2026-09-19 18:04:29 +0530

    Fix Xora homepage route

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const app = express();
 
 app.use(express.json());
 app.use(express.static("public"));
-
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 // =====================================
 // XORA MEMORY
